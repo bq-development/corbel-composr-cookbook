0be8bbeda636bced5015ece123ccf80822d6fc20
javier.heredia <javier.heredia@bq.com>
2015-04-29 12:49:11 +0200

    Add tag support and remove image before launch container

--- recipes/container.rb
+++ recipes/container.rb
@@ -1,15 +1,34 @@
+# Variables
 image = node[:composer][:docker][:image_name]
-port = node[:composer][:docker][:port]
+image_tag = node[:composer][:docker][:image_tag]
+http_port = node[:composer][:docker][:http_port]
+transport_port = node[:composer][:docker][:transport_port]
 detach = node[:composer][:docker][:detach]
 container_name = node[:composer][:docker][:container_name]
 volume = node[:composer][:docker][:volume]
-# Pull latest image
-docker_image image
 
-# Run container exposing ports
+# Stop/Remove Container
+docker_container container_name do
+  action :stop
+end
+docker_container container_name do
+  action :remove
+end
+# Delete image
+docker_image image do
+  tag image_tag
+  action :remove
+end
+# Pull tagged image
+docker_image image do
+  tag image_tag
+end
+
+# Run container
 docker_container image do
   detach detach
-  port port
+  port http_port
+  port transport_port
   container_name container_name
   volume volume 
 end
